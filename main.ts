e7d003806b0272f5a3bd15e3684c356e73a6b6c5
avishahi <87566871+avishahi@users.noreply.github.com>
2021-07-27 15:49:59 +0530

    Update pxt.json, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ namespace screenmagic {
     //* blockId= screenmagicplotat
     //* block="plot at $index"
     //% index.min=0 index.max=25
-    export function plotAt(index: number): void {
+    export function plotmatrix(index: number): void {
         index |= 0
         const y = Math.floor(index / 5);
         const x = Math.floor(index % 5);
